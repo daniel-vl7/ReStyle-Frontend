7947ac96af230bf5a72c23b522b8e8817ed0712e
María Alejandra Díaz Villacrez <u202118315@upc.edu.pe>
2024-04-29 23:46:58 -0500

    feat(remodeler-detail): added remodeler-detail feature
    added remodeler-detail component to feature/remodeler-detail branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-foun
 import {HomeComponent} from "./public/pages/home/home.component";
 import {RemodelerSearchComponent} from "src/app/reStyle/remodeler/components/remodeler-search/remodeler-search.component.ts";
 import {ComingSoonComponent} from "./public/pages/coming-soon/coming-soon.component";
+import {RemodelerDetailComponent} from "/src/app/reStyle/remodeler/components/remodeler-detail/remodeler-detail.component.ts";
 
 export const routes: Routes = [
   {path:'', pathMatch: 'full', redirectTo: 'home'},
   {path: 'remodelers', component: RemodelerSearchComponent},
+  {path: 'remodelers/:id', component: RemodelerDetailComponent},
   {path: 'coming-soon', component: ComingSoonComponent},
   {path:'home',  component: HomeComponent},
   {path: '**', component: PageNotFoundComponent},
---
+++ src/app/reStyle/remodeler/components/remodeler-detail/remodeler-detail.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common'
+
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCardModule} from "@angular/material/card";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatFormFieldModule} from "@angular/material/form-field";
+
+import {RemodelerApiService} from "/src/app/reStyle/remodeler/services/remodeler-api.service.ts";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-remodeler-detail',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTabsModule,
+    MatFormFieldModule
+  ],
+  templateUrl: './remodeler-detail.component.html',
+  styleUrl: './remodeler-detail.component.css'
+})
+export class RemodelerDetailComponent implements OnInit{
+  data: any = {};
+  remodeler: any = {};
+
+  constructor(private remodelerApiService: RemodelerApiService, private router: Router) {
+  }
+  ngOnInit(){
+    let id = this.router.url.split('/')[2];
+    this.getRemodelerById(id);
+  }
+
+  getRemodelerById(id: any):void{
+    this.remodelerApiService.getRemodelerById(id).subscribe((data:any)=>{
+        this.remodeler = data;
+      },
+      (error:any)=>{
+        console.log(error);
+      });
+  }
+}
