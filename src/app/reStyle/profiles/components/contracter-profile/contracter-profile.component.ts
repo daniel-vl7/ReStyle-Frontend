7b3ddadb6d81f863164c677c16b09d3e83fae70d
Janover Saldaña <u20201b510@upc.edu.pe>
2024-05-01 13:47:11 -0500

    feat(profiles): Added user profile structure.

---
+++ src/app/reStyle/profiles/components/contracter-profile/contracter-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contracter-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './contracter-profile.component.html',
+  styleUrl: './contracter-profile.component.css'
+})
+export class ContracterProfileComponent {
+
+}
---
+++ src/app/reStyle/profiles/components/remodeler-profile/remodeler-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-remodeler-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './remodeler-profile.component.html',
+  styleUrl: './remodeler-profile.component.css'
+})
+export class RemodelerProfileComponent {
+
+}
