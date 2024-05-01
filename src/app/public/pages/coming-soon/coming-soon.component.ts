8f9969fde011ccce3526430aafc3eb7337ce4690
daniel <u202121440@upc.edu.pe>
2024-05-01 18:13:25 -0500

    feat(profiles): Add public components in coming-soon component

--- src/app/public/pages/coming-soon/coming-soon.component.ts
+++ src/app/public/pages/coming-soon/coming-soon.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {SidebarComponent} from "../../components/sidebar/sidebar.component";
+import {ToolbarComponent} from "../../components/toolbar/toolbar.component";
 
 @Component({
   selector: 'app-coming-soon',
   standalone: true,
-  imports: [],
+  imports: [
+    SidebarComponent,
+    ToolbarComponent
+  ],
   templateUrl: './coming-soon.component.html',
   styleUrl: './coming-soon.component.css'
 })
