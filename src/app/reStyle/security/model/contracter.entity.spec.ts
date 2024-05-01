0cf7035c24439df7a3bc71df44e0fe6b4a688e91
Janover Saldaña <u20201b510@upc.edu.pe>
2024-05-01 00:43:31 -0500

    feat(security): added security architecture.

---
+++ src/app/reStyle/security/model/contracter.entity.spec.ts
@@ -0,0 +1,7 @@
+import { ContracterEntity } from './contracter.entity';
+
+describe('ContracterEntity', () => {
+  it('should create an instance', () => {
+    expect(new ContracterEntity()).toBeTruthy();
+  });
+});
---
+++ src/app/reStyle/security/model/remodeler.entity.spec.ts
@@ -0,0 +1,7 @@
+import { RemodelerEntity } from './remodeler.entity';
+
+describe('RemodelerEntity', () => {
+  it('should create an instance', () => {
+    expect(new RemodelerEntity()).toBeTruthy();
+  });
+});
